dd9fdbda8f1d800b139b774bd89a47b2dff83147
Anuradha2k21 <61109105+Anuradha2k21@users.noreply.github.com>
2024-03-31 13:06:43 +0530

    first commit

---
+++ app/src/main/java/com/example/mad_cw/UserRegister.java
@@ -0,0 +1,14 @@
+package com.example.mad_cw;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UserRegister extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_register);
+    }
+}
