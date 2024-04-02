97a1343285f33da9fc42c45dcf14dc4dbd616851
Anuradha2k21 <61109105+Anuradha2k21@users.noreply.github.com>
2024-04-02 16:48:40 +0530

    log-in ui added

--- app/src/main/java/com/example/mad_cw/user/UserLogin.java
+++ app/src/main/java/com/example/mad_cw/user/UserLogin.java
@@ -1,6 +1,9 @@
 package com.example.mad_cw.user;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,16 +14,14 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.mad_cw.R;
 
 public class UserLogin extends AppCompatActivity {
+    private LinearLayout adminClick;
+    private EditText email, password;
+    private Button login, register, guest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_user_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
 }
