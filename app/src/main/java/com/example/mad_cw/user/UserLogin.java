4f28c8b3baf29c8adce894573ca52e88c8efed61
Anuradha2k21 <61109105+Anuradha2k21@users.noreply.github.com>
2024-04-02 17:06:06 +0530

    connected register, guestview into the login screen

--- app/src/main/java/com/example/mad_cw/user/UserLogin.java
+++ app/src/main/java/com/example/mad_cw/user/UserLogin.java
@@ -1,8 +1,13 @@
 package com.example.mad_cw.user;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
@@ -12,16 +17,69 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.mad_cw.R;
+import com.example.mad_cw.course.CourseRecyclerView;
 
 public class UserLogin extends AppCompatActivity {
     private LinearLayout adminClick;
-    private EditText email, password;
-    private Button login, register, guest;
+    private EditText etEmail, etPassword;
+    private Button btnLogin, btnRegister, btnGuest;
+    private ImageButton btnViewPassword;
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_user_login);
+
+        btnViewPassword = findViewById(R.id.btn_view_password);
+        etPassword = findViewById(R.id.txt_pword);
+        etEmail = findViewById(R.id.txt_email);
+        btnLogin = findViewById(R.id.btn_login);
+        btnRegister = findViewById(R.id.btn_register_redirect);
+        btnGuest = findViewById(R.id.btn_guest);
+        adminClick = findViewById(R.id.admin_click);
+
+        btnViewPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (etPassword.getTransformationMethod().equals(PasswordTransformationMethod.getInstance())) {
+                    btnViewPassword.setImageResource(R.drawable.ic_view_password);
+                    etPassword.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                } else {
+                    btnViewPassword.setImageResource(R.drawable.ic_hide_password);
+                    etPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                }
+            }
+        });
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(UserLogin.this, UserRegister.class);
+                startActivity(intent);
+            }
+        });
+        btnGuest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(UserLogin.this, CourseRecyclerView.class);
+                startActivity(intent);
+            }
+        });
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent intent = new Intent(UserLogin.this, CourseRecyclerView.class);
+//                startActivity(intent);
+            }
+        });
+        adminClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent intent = new Intent(UserLogin.this, AdminLogin.class);
+//                startActivity(intent);
+            }
+        });
+
     }
 }
